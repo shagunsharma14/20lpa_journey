a032b7936c72c16370414720abc4bc7b74926027
Shagun Sharma <shaguns880@gmail.com>
2026-01-14 00:22:49 +0000

    Day 1: Created my first automation script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Starting System Check.."
+whoami
+date
+echo "Creating a test file..."
+touch testfile.txt
+ls -l testfile.txt
+echo "Check complete. I am a future Cloud Engineer."
